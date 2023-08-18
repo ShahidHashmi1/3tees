f09e9e1db45493451f62304d63cc079616410674
unknown <hashmi.pgh@gmail.com>
2023-08-18 19:17:35 -0400

    added bag component

---
+++ src/app/bagdialog/bagdialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bagdialog',
+  templateUrl: './bagdialog.component.html',
+  styleUrls: ['./bagdialog.component.scss']
+})
+export class BagdialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
